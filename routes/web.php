e2e7c9706e80c2b6cb3312e179eeb4bffbc86d20
Mustafa Karrokhi <mustafa.karrokhi@gmail.com>
2025-04-25 23:50:18 +0300

    Finished Video named: Create & Display Views

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 
-// USING THE ROUTE METHOD from Oreilly Training called: Laravel from Scratch
+// Oreilly Training : Laravel from Scratch
+// Chapter 4: Views & Controllers
+// Video named: Create & Display Views
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs', function () {
-    return '<h1>Available Jobs</h1>';
+    return view('jobs.index');
 })->name('jobs');
 
 
-// Video named: Response Helper
-// Route::get('/test', function() {
-//     return response("You are Hafiz Quran", 200);
-// });
-
-// // Minute 3:18 Content Type with the request
-// Route::get('/test', function() {
-//     // return response("<h1>You are Hafiz Quran</h1>", 200)->header('Content-Type', 'text/plain');
-//     return response("<h1>You are Hafiz Quran</h1>", 200)->header('Content-Type', 'text/html');
-// });
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
 
 
-Route::get('/notfound', function() {
-    return response('Page Not Found', 404);
-});
 
-// Minute 4:15 Content Type with the request
-Route::get('/test', function() {
-    return response()->json(['name' => 'Mustafa Karrokhi', 'age' => 40])->cookie('name', 'Mustafa Karrokhi');
-});
-
-Route::get('/download', function () {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/read-cookie', function(Request $request) {
-    $cookieValue = $request->cookie('name');
-    return response()->json(['cookie' => $cookieValue]);
-});
